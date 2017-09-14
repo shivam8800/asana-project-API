57ccfb435cbd18ba1d32658f79f46c15c9f06ac8
shivam8800 <shivam16@navgurukul.org>
2017-09-14 18:17:45 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import Hapi from 'hapi';
+
+const server = new Hapi.Server();
+
+server.connection({
+	port: 8000
+});
+
+server.route({
+	method: 'GET',
+	path: '/hello',
+	handler: ( request, reply) =>{
+		reply('Hello World!');
+	}
+});
+
+server.start(err => {
+
+    if (err) {
+
+        // Fancy error handling here
+        console.error( 'Error was handled!' );
+        console.error( err );
+
+    }
+
+    console.log( `Server started at ${ server.info.uri }` );
+
+});
